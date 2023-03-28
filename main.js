14673165fa65b9295a2a8cddef5809646986d353
A3Barbosa <a3barbosa21@gmail.com>
2023-03-28 17:55:51 -0400

    code day2 week3

--- main.js
+++ main.js
@@ -7,3 +7,9 @@
 multiply = function (a,b){
   return a * b;
 }
+
+//We need a function that can transform a number (integer) into a string.
+
+function numberToString(num) {
+  return num.toString()
+}
